e24bea904d2a1f987ebd89656fb232c19ec8f7b4
Benjamin Lavena <benja@zmaster.com.ar>
2022-08-07 22:49:58 -0300

    Fixed cops warnings

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
+# Application Helper class
 module ApplicationHelper
   def flash_css_class(type)
     case type
-      when 'notice' then 'alert-success'
-      when 'alert' then 'alert-danger'
-      else 'alert-primary'
+    when 'notice' then 'alert-success'
+    when 'alert' then 'alert-danger'
+    else 'alert-primary'
     end
   end
 end
